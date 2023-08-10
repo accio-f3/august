0ceb9ae6cf21f5488e28c0793b112015713dde22
accio-f3 <acciof3@gmail.com>
2023-08-10 21:14:11 +0530

    4th class - 1st half

---
+++ prototypicalInheritance.js
@@ -0,0 +1,31 @@
+// let obj1={
+//     // nam:'pc',
+//     // city: 'Blr',
+//     // getDetails(){
+//     //     console.log(`Hi I am ${this.nam} and from ${this.city}`);
+//     // }
+// }
+
+// let obj2 = {
+//     nam:'Prikshit',
+//     city: 'delhi',
+//     getDetails(){
+//         console.log(`Hi I am ${this.nam} and from ${this.city}`);
+//     }
+// }
+
+// // obj1.__proto__ = obj2;
+// let obj={
+
+// }
+
+// obj.__proto__=obj2;
+// obj1.getDetails();
+
+// __proto__
+// prototype
+
+// prototype infinite loop
+// Object.prototype.__proto__ = obj1.__proto__
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/proto
+
